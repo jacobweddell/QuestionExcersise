06a6c9ab4b44e11eac23ba55670fefc05a3ab0ba
MMR\J.Weddell <j.weddell@mmr-research.com>
2023-12-19 09:42:45 +0000

    Fix naming

--- QuestionExcersise/ViewModels/QuestionViewModel.cs
+++ QuestionExcersise/ViewModels/QuestionViewModel.cs
@@ -3,8 +3,8 @@
     public class QuestionViewModel
     {
         public int Number { get; set; }
-        public string Text { get; set; }
-        public string Answer { get; set; }
+        public string Name { get; set; }
+        public string Response { get; set; }
 
     }
 }
